21be5c5242a5d4901194f2a230234dbed77ceba1
habibmhamadi <habibmhamadi@gmail.com>
2021-08-06 10:05:14 +0430

    Update README.md

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col items-center">
                 <div class="bg-white w-3/4 p-3 overflow-hidden shadow-sm sm:rounded-lg">
-                   <h1 class="text-2xl">{{__('Laravel Roadmap Beginner Challenge')}}</h1>
+                   <h1 class="text-2xl">{{ config('app.name', 'Simple Personal Blog') }} ({{__('Laravel Roadmap Beginner Challenge')}})</h1>
                     <br>
                     <p>
                         {{ __('Thanks') }} <a class="text-blue-500 hover:text-blue-700 hover:underline" href="https://github.com/laravelDaily">LaravelDaily</a>
