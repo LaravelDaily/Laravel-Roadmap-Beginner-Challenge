33c16b4469b33894e6bc163125c2f46721d5ee66
pshko <aram.khorsandi@gmail.com>
2021-08-08 19:08:41 +0430

    install breeze and remove register route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,13 +25,13 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
-                        @if (Route::has('register'))
+{{--                        @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
+                        @endif--}}
                     @endauth
                 </div>
             @endif
