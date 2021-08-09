5a4677acaaae8a24f0221dc042556deaad067081
RJUSM <rustam190401@mail.ru>
2021-08-09 12:53:06 +0300

    UPDATE : models, add fillable

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $table = 'articles';
+    protected $fillable = ['title', 'fulltext', 'image'];
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $table = 'tags';
+    protected $fillable = ['name'];
 }
