fbac565a939f492b37c08bef8416107b00079a76
RJUSM <rustam190401@mail.ru>
2021-08-09 13:04:25 +0300

    CREATE : relations one-to-many(hasMany), and one-to-one (hasOne)

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,11 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Article extends Model
 {
     use HasFactory;
 
+    /**
+     * @var string
+     */
     protected $table = 'articles';
+
+    /**
+     * @var string[]
+     */
     protected $fillable = ['title', 'fulltext', 'image'];
+
+    /**
+     * Using plural naming(tags) because Article may have many Tag(s)
+     * @return HasMany
+     */
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
+
+    /**
+     * Using singular naming(category) because Article may have only one Category
+     * @return HasOne
+     */
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class);
+    }
 }
