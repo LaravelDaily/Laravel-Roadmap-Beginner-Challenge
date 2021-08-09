0e01fc86d98a15b69d6a68e62f28a306f5ab4a62
RJUSM <rustam190401@mail.ru>
2021-08-09 13:34:07 +0300

    UPDATE : path to the "home" route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/article';
 
     /**
      * The controller namespace for the application.
