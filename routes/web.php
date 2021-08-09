276648f055e0e8de1e6d9634ee49e792fd36ca42
RJUSM <rustam190401@mail.ru>
2021-08-09 12:44:56 +0300

    UPDATE : Dashboard route, configure route group

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
