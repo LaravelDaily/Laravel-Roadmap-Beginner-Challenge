e6fda73ddbcf37350a3e0cabac5eb2f0254db4c1
pshko <aram.khorsandi@gmail.com>
2021-08-18 16:06:35 +0430

    tag update

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Panel\Tag\CreateTagRequest;
+use App\Http\Requests\Panel\Tag\UpdateTagRequest;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
@@ -41,13 +42,18 @@ class TagController extends Controller
 
     public function edit(Tag $tag)
     {
-        //
+        return view('panel.tag.edit', compact('tag'));
     }
 
 
-    public function update(Request $request, Tag $tag)
+    public function update(UpdateTagRequest $request, Tag $tag)
     {
-        //
+        $data = $request->validated();
+        $tag->update(
+            $data
+        );
+        session()->flash('status', 'Tag Updated Successfully!');
+        return redirect(route('tags.index'));
     }
 
 
---
+++ app/Http/Requests/Panel/Tag/UpdateTagRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Panel\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'max:255', 'string', Rule::unique('tags')->ignore($this->tag)]
+        ];
+    }
+}
