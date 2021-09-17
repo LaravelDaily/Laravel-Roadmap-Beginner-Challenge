b6e5453643492dd7349047fe182056fa4cd9a98c
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-17 22:48:21 +0800

    added default email and password in UserFactory and hashed default password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => 'admin@admin.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
