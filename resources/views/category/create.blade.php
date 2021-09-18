1e1ec5be86f97d101f0c69fafbe4e4bc63c4cc1e
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-18 20:52:36 +0800

    added category.create view

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.layout')
+
+@section('title')
+    Categories
+@endsection
+
+@section('content')
+    <main class="container mt-5 px-5">
+        <section class="container border rounded px-0">
+            <h4 class="border-bottom px-4 py-2 bg-secondary bg-opacity-10">Add Category</h4>
+            <form action="{{ route('category.store') }}" method="POST" class="container py-3">
+                @csrf
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="text" class="form-control {{ $errors->has('name') ? 'border-danger' : '' }}" name="name"
+                        value="{{ old('name') }}">
+                    @error('name')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button class="btn btn-secondary">Submit</button>
+            </form>
+        </section>
+    </main>
+@endsection
