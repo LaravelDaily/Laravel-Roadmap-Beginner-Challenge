3087e690e72f4fbdb90f15a10a15cf14a4f27f05
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-21 23:06:29 +0800

    added User model hasMany relationship to Article model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
