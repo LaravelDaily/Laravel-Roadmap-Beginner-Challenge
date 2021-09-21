ed1757da8bfc0f312b27a7711a092c6d52ec8ad6
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-21 23:06:49 +0800

    added guest.about view

---
+++ resources/views/guest/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.layout')
+
+@section('title')
+    About
+@endsection
+
+@section('content')
+    <main class="container mt-5 px-5">
+        <section class="container border rounded px-0">
+            <h4 class="border-bottom px-4 py-2 bg-secondary bg-opacity-10">About Us</h4>
+            <div class="container">About us details</div>
+        </section>
+    </main>
+@endsection
