fd41a1c86bb9f57c756b7bc742750795583d1749
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-22 17:06:06 +0800

    replaced image_path to image in CreateArticlesTable migration

--- database/migrations/2021_09_17_084450_create_articles_table.php
+++ database/migrations/2021_09_17_084450_create_articles_table.php
@@ -19,7 +19,7 @@ class CreateArticlesTable extends Migration
             $table->unsignedBigInteger('category_id')->constrained();
             $table->string('title');
             $table->text('body');
-            $table->string('image_path')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
