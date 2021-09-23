34b839846482036908b4f5284f916be761cd4d1b
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-23 17:28:13 +0800

    renamed category folder to categories folder for categories Resource route

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -7,15 +7,13 @@
 @section('content')
     <main class="container mt-5 px-5">
         <section class="container border rounded px-0">
-            <h4 class="border-bottom px-4 py-2 bg-secondary bg-opacity-10">Edit Category</h4>
-            <form action="{{ route('category.update', ['category' => $category->id]) }}" method="POST"
-                class="container py-3">
+            <h4 class="border-bottom px-4 py-2 bg-secondary bg-opacity-10">Add Category</h4>
+            <form action="{{ route('categories.store') }}" method="POST" class="container py-3">
                 @csrf
-                @method('PUT')
                 <div class="mb-3">
                     <label class="form-label">Name</label>
                     <input type="text" class="form-control {{ $errors->has('name') ? 'border-danger' : '' }}" name="name"
-                        value="{{ $category->name }}">
+                        value="{{ old('name') }}">
                     @error('name')
                         <p class="text-danger">{{ $message }}</p>
                     @enderror
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,7 +10,7 @@
             <div class="alert alert-danger" role="alert">{{ $message }}</div>
         @enderror
 
-        <a href="{{ route('category.create') }}" class="btn btn-success">Add Category</a>
+        <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
         <section class="container border rounded px-0 mt-3">
             <h4 class="border-bottom px-4 py-2 bg-secondary bg-opacity-10">Categories</h4>
             <ul class="list-group">
@@ -19,10 +19,10 @@
                     <li class="list-group-item container d-flex">
                         <p class="col-6">{{ $category->name }}</p>
                         <div class="mx-3">
-                            <a href="{{ route('category.edit', ['category' => $category->id]) }}"
+                            <a href="{{ route('categories.edit', ['category' => $category->id]) }}"
                                 class="btn btn-primary">Edit</a>
                         </div>
-                        <form action="{{ route('category.destroy', ['category' => $category->id]) }}" method="POST">
+                        <form action="{{ route('categories.destroy', ['category' => $category->id]) }}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger">Delete</button>
