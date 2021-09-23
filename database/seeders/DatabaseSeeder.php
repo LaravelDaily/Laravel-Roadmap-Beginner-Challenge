858d63ab667c976d11530d6eb5dca96df2a594a6
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-23 10:49:42 +0800

    added CategorySeeder, TagSeeder and ArticleSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            CategorySeeder::class,
+            TagSeeder::class,
+            ArticleSeeder::class
         ]);
     }
 }
