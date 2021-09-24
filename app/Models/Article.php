c40305039b072dc72799d3f2bb6c27718b71f97c
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-09-24 12:10:56 +0800

    added fillable properties to Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,6 +9,14 @@ class Article extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'body',
+        'image'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
