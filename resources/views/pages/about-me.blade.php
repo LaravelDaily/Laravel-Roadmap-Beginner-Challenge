404063a4a441fbfba4ddfe8c14a1b63ecb5987b5
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-19 13:03:03 -0300

    add about-me page resource

---
+++ resources/views/pages/about-me.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.site-layout')
+
+
+@section('site-title')
+    <title>About Me</title>
+@endsection
+
+
+
+@section('content')
+    <div class=" w-100 d-flex flex-column align-items-center mt-4">
+        <div>
+            <h3>Hello, Im' Denis Feliciano</h3>
+            <p>I'm currently in São Paulo city in Brazil, and i'm fullstack developer
+                This website is developed for the <a
+                    href="https://github.com/LaravelDaily/Laravel-Roadmap-Beginner-Challenge"
+                    target="blank">Laravel-Roadmap-Beginner-Challenge</a>.</p>
+            <p>To learn more about the challenge visit the link above</p>
+        </div>
+    </div>
+@endsection
