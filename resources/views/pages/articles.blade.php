d51ffbc582d67060c79e2c550f19bf3e1bdff871
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-19 13:02:54 -0300

    add article index page resource

---
+++ resources/views/pages/articles.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.site-layout')
+
+
+@section('site-title')
+    <title>Articles</title>
+@endsection
+
+
+@section('content')
+    <div class=" w-100 d-flex flex-column align-items-center mt-4">
+        <section>
+            <h1>List of articles</h1>
+        </section>
+    </div>
+@endsection
