1fedfc7d782af3525bc8d00c37d3a0242c01ce2f
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-19 15:22:59 -0300

    add articles, categories and tags manage links to dashboard

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <a href="{{ route('article_manager') }}">Manage Articles</a>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="#">Manage Categories</a>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="#">Manage Tags</a>
                 </div>
             </div>
         </div>
