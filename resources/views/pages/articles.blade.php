dd847267b21889a9bd6aa33e0f198abde34e0987
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-20 15:22:11 -0300

    add image src from a url and update article view

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -9,19 +9,21 @@
 @section('content')
     <div class=" w-100 d-flex flex-column align-items-center mt-4">
         <section class="w-75">
-            <h1 class="text-center">List of articles</h1>
+            <h1 class="text-center py-4">List of articles</h1>
             <ul class="mt-4">
                 @foreach ($articles as $article)
-                    <li class="d-flex">
-                        <img class="w-25 h-25"
-                            src="data:image/jpg;charset=utf8;base64,{{ base64_encode($article->image) }}" />
+                    <li class="d-flex justify-between align-items-center">
+                        <img class="w-25 h-25  m-4 border" src={{ asset("storage/$article->image") }} />
 
 
                         <div>
-                            <a href="{{ route('article-show', $article->id) }}">
-                                <h3>{{ $article->title }}</h3>
+                            <a class="text-decoration-none text-dark" href="{{ route('article-show', $article->id) }}">
+                                <h3 class="text-sm">{{ $article->title }}</h3>
                             </a>
-                            <p>{{ $article->full_text }}</p>
+                            <div class=" flex">
+                                <p class="overflow-hidden" style="height: 90px">{{ $article->full_text }}</p>
+                                <a href="{{ route('article-show', $article->id) }}">read more.</a>
+                            </div>
                         </div>
 
                     </li>
