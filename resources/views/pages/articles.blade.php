68be9af08b9af206df1d6cc518658b4b3e6054b8
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-20 02:13:00 -0300

    add articles page

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -8,8 +8,26 @@
 
 @section('content')
     <div class=" w-100 d-flex flex-column align-items-center mt-4">
-        <section>
-            <h1>List of articles</h1>
+        <section class="w-75">
+            <h1 class="text-center">List of articles</h1>
+            <ul class="mt-4">
+                @foreach ($articles as $article)
+                    <li class="d-flex">
+                        <img class="w-25 h-25"
+                            src="data:image/jpg;charset=utf8;base64,{{ base64_encode($article->image) }}" />
+
+
+                        <div>
+                            <a href="{{ route('article-show', $article->id) }}">
+                                <h3>{{ $article->title }}</h3>
+                            </a>
+                            <p>{{ $article->full_text }}</p>
+                        </div>
+
+                    </li>
+                    <hr>
+                @endforeach
+            </ul>
         </section>
     </div>
 @endsection
