6a6ea1cd0657c38f8fb2cdd60e9b4d9446782179
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-20 02:14:39 -0300

    add category edit resource view

---
+++ resources/views/pages/dashboard/category/category-edit.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <a href="{{ route('dashboard') }}">{{ __('Dashboard ') }}</a>
+            <span>\</span>
+            <a href="{{ route('category.index') }}">{{ __('Manage Category ') }}</a>
+            <span>\</span>
+            <a href="{{ route('category.edit', $category->id) }}">{{ __('Edit Category ') }}</a>
+        </h2>
+    </x-slot>
+
+    <div class="py-12 grid place-items-center">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <form method="post" action="{{ route('category.update', $category->id) }}" enctype="multipart/form-data">
+                @csrf
+                <div class="flex flex-col">
+                    <div class="flex flex-col">
+                        <input class="rounded" id="category_name" type="text" name="category_name"
+                            placeholder="Category name" value="{{ $category->name }}">
+                        @error('category_name')
+                            <div class="p-1 bg-red-200 text-sm">{{ $message }}</div>
+                        @enderror
+
+                        <button class="border p-2 bg-green-500 rounded text-white mt-2">Update Category</button>
+                    </div>
+            </form>
+
+        </div>
+    </div>
+</x-app-layout>
