724a4484dca0b21510dc9ccaddfdc277f7f08f30
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-20 02:14:53 -0300

    add category manage view

--- resources/views/pages/dashboard/category/category-index.blade.php
+++ resources/views/pages/dashboard/category/category-index.blade.php
@@ -1,23 +1,41 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard - Manage Category') }}
+            <a href="{{ route('dashboard') }}">{{ __('Dashboard ') }}</a>
+            <span>\</span>
+            <a href="{{ route('category.index') }}">{{ __('Manage Category ') }}</a>
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="{{ route('category.create') }}">Create new Category</a>
+            <a href="{{ route('category.create') }}" class=" overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-green-200 border-b border-gray-200 text-center">
+                    <strong>
+                        Create new Category
+                    </strong>
                 </div>
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="#">Edit Category</a>
-                </div>
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="#">Delete Category</a>
-                </div>
-            </div>
+
+            </a>
+            <ul class="mt-10">
+                @foreach ($categories as $category)
+                    <li
+                        class="py-6 pl-6 pr-14 bg-white border-b border-gray-200 rounded flex justify-between align-items-center">
+                        <a class="text-blue-500"
+                            href="{{ route('category.show', $category->id) }}">{{ $category->name }}</a>
+
+                        <div class="flex ">
+                            <a href="{{ route('category.edit', $category->id) }}"
+                                class="border py-1 px-3 bg-gray-700 text-white rounded mr-2">Edit</a>
+                            <form action="{{ route('category.destroy', $category->id) }}" method="POST">
+                                @csrf
+                                <button type="submit"
+                                    class="border py-1 px-3 bg-red-500 text-white rounded">Delete</button>
+                            </form>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 </x-app-layout>
