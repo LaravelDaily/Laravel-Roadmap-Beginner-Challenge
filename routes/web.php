80b93f4852739fc560df7fbd1ffb205b14b94bcb
mbecovic <becovic@protonmail.com>
2021-10-22 23:30:18 +0200

    - added routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (){
+    $articles = Article::paginate(10);
+    return view('index', compact('articles'));
+})->name('index');
+
+Route::get('/article/{id}', [PageController::class, 'show'])->name('article.show');
+
+Route::view('about', 'about')->name('about');
+
+Route::middleware('auth')->prefix('admin')->group(function(){
+    Route::resource('articles', ArticleController::class);
+    Route::resource('categories', CategoryController::class)->except(['show']);
+    Route::resource('tags', TagController::class)->except(['show']);
+
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
