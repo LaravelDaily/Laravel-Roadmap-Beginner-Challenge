d597d1f19d4102342e24fde28ba85bd719f9aba3
mbecovic <becovic@protonmail.com>
2021-10-22 23:20:47 +0200

    - added requests for validating form input

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'text' => 'required',
+            'category_id' => 'nullable|exists:categories,id',
+            'tags' => 'array',
+            'image' => 'nullable|mimes:jpg,jpeg,bmp,png,gif'
+        ];
+    }
+}
