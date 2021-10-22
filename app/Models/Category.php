c4c6d3c7faa314db75a2de22ef364d181bf3577e
mbecovic <becovic@protonmail.com>
2021-10-22 23:20:05 +0200

    - models with relationships

---
+++ app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = ['name'];
+
+    public function articles(){
+        return $this->hasMany(Article::class);
+    }
+}
