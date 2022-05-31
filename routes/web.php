03835ef40b6f2023f4d757e5205fcbbe7a7b5002
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-05-30 19:44:55 -0600

    [EDIT]web.php: added all routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\DetachTagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,13 +15,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['middleware'=>'auth'],function(){
+    Route::delete('/tagdetach/{article}/{tag}',DetachTagController::class)->name('tag.detach');
+    // Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::resource('category', CategoryController::class)->except(['show']);
+    Route::resource('article', ArticleController::class)->except(['show','index']);
+});
+Route::resource('article', ArticleController::class)->only('show','index');
+Route::view('/aboutme', 'aboutme')->name('aboutme');
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
