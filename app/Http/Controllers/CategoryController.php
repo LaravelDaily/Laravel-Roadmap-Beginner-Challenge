5aa1c4043ed3fd27ecc15f24bf848644e49c8a31
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-05-30 19:55:49 -0600

    [ADD]CategoryController

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $search=request()->search;
+        if(isset($search)){
+            $categories=Category::select(['id','name'])->where('name','LIKE','%'.$search.'%')->latest()->simplePaginate(4);
+            return view('category.index',compact('categories'));
+        }
+        $categories=Category::select(['id','name'])->latest()->simplePaginate(4);
+        return view('category.index',compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('category.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCategoryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        //
+        $validated=$request->validated();
+        Category::create($validated);
+        return redirect()->route('category.index');     
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {  
+        return view('category.edit',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCategoryRequest  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $validated=$request->validated();
+        $category->update($validated);
+        return redirect()->route('category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('category.index');
+    }
+}
