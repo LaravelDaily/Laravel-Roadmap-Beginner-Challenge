dcc6b2d090f0116e12ee49d0117d737af6120354
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-05-31 21:00:52 -0600

    [ADD]CategoryFactory/Seeder
    [ADD]CategoryFactory/CategorySeeder
    
    [PATCH]CategoryFactory

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\Models\Category::factory(3)->create();
+    }
+}
