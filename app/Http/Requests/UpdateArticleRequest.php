c2340cec868eb1588092b89cf749f9ec2d9577bb
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-06-09 17:35:39 -0600

    [ADD]Store/UpdateArticleRequest

---
+++ app/Http/Requests/UpdateArticleRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'=>['required','string'],
+            'text'=>['required','string'],
+            'file_path'=>['nullable','image'],
+            'category_id'=>'required'
+        ];
+    }
+    public function messages()
+    {
+        return[
+            'category_id.numeric'=>'Select a category',
+        ];
+    }
+    public function sanitizedTags()
+    {
+
+        return $this->tag?array_map('trim',explode(',',$this->tag)):null;
+    }
+}
