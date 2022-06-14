c96a64ad64a5407c5045c5b7840615d18a93b40d
sangeethkc <keyceesangi@gmail.com>
2022-06-14 21:06:44 +0530

    now all done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,11 +59,11 @@
                                 </li>
                             @endif
 
-                            @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
