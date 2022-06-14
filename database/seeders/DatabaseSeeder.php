f78c6e9ec15f496ce3b3d80e88a68974c6f1dd18
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-06-13 18:27:43 -0600

    [EDIT]DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CategorySeeder::class,
+            // TagSeeder::class,
+            ArticleSeeder::class
+        ]);
     }
 }
