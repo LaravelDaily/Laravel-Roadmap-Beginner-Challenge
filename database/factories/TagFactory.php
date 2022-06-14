545774b7cdc97cccde8b79ee44caff917bf4aade
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-06-13 18:26:17 -0600

    [ADD]TagFactory
    [PATCH]TagFactory: unique names
    
    TagFactory

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -14,7 +14,7 @@ class TagFactory extends Factory
     public function definition()
     {
         return [
-            'name'=>$this->faker->word(),
+            'name'=>$this->faker->unique(true)->word(),
         ];
     }
 }
