372e2c70d7d9f465efb561562e04beddbf8315df
sangeethkc <keyceesangi@gmail.com>
2022-06-22 16:29:30 +0530

    a bit of styling on posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with('category', 'tags')->get();
+        $posts = Post::with('category', 'tags')->latest()->get();
 
         return view('posts.index', compact('posts'));
     }
