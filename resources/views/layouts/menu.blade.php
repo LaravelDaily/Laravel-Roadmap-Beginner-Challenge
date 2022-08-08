3528f44a730b4608d6f3e7c24f37865ba0a6b55f
Thananjeyan <thanjeys@gmail.com>
2022-08-08 14:53:20 +0530

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,7 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link active">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+</li>
