605eeb67961197e639c6150138ff46d4c74a1806
Gardi <ahmadgardi0@gmail.com>
2022-08-11 23:04:34 +0300

    upload missing files

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -67,17 +67,6 @@ class ArticlesController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
