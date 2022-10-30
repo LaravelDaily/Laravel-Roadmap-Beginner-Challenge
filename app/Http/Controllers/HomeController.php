227adad7c666fb474cd204a19dc05c85a0c5f378
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-10-30 18:41:49 +0100

    Add return homepage view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,8 +12,8 @@ class HomeController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        return view('welcome');
     }
 }
