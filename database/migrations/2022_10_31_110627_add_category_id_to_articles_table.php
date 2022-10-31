ae489bd9bb6d9638b0febfecab7069ff5e8ea69b
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-10-31 12:10:28 +0100

    Add category_id to articles table

---
+++ database/migrations/2022_10_31_110627_add_category_id_to_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->foreignId('category_id')->after('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+};
