7644e029be0f6953abb5611c9b7095a18494627a
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-10-31 12:44:05 +0100

    Add timestamps to tags and categories table

--- database/migrations/2022_10_30_173726_create_tags_table.php
+++ database/migrations/2022_10_30_173726_create_tags_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->timestamps();
         });
     }
 
