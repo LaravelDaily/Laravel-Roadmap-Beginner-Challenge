67f2281d86e1c105f62826562674c00bb877454f
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-01 08:54:08 +0100

    Get article data to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,6 +15,7 @@ class HomeController extends Controller
      */
     public function __invoke()
     {
-        return view('home');
+
+        return view('home')->with('articles', Article::with('tags')->get());
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,22 +18,21 @@
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
-                @for ($i = 0; $i < 5; $i++)
-                    <!-- Post preview-->
+                @foreach ($articles as $article)
                 <div class="post-preview">
                     <a href="post.html">
-                        <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                        <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                        <h2 class="post-title">{{ $article->title }}</h2>
+                        <h3 class="post-subtitle">{{ $article->excerpt }}</h3>
                     </a>
                     <p class="post-meta">
                         Posted by
-                        <a href="#!">Start Bootstrap</a>
-                        on September 24, 2022
+                        <a href="#!">{{ $article->user->name }}</a>
+                        on {{ $article->created_at->format('F d, Y') }}
                     </p>
                 </div>
                 <!-- Divider-->
                 <hr class="my-4" />
-                @endfor
+                @endforeach
     
                 <!-- Pager-->
                 <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
