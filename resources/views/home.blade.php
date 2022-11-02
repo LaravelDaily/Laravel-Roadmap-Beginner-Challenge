c49b4f69b06c01828139d8a8396cf32bd05aa009
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-02 13:07:10 +0100

    Add route to article show and auth site heading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,13 @@
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <div class="site-heading">
-                        <h1>Personal Blog</h1>
+                        @auth
+                        <h1>{{ auth()->user()->name }}</h1>
+                        @else
+                        <h1>Personal blog</h1>
+                        @endauth
                         <span class="subheading">A Blog for developers</span>
+                        <a href="{{ route('article.create') }}" class="btn btn-dark btn-lg mt-4">CREATE ARTICLE</a>
                     </div>
                 </div>
             </div>
@@ -20,7 +25,7 @@
             <div class="col-md-10 col-lg-8 col-xl-7">
                 @foreach ($articles as $article)
                 <div class="post-preview">
-                    <a href="post.html">
+                    <a href="{{ route('article.show', $article) }}">
                         <h2 class="post-title">{{ $article->title }}</h2>
                         <h3 class="post-subtitle">{{ $article->excerpt }}</h3>
                     </a>
