cf7e7d059f3a06f788704a023eb8f1bd8a5ae2d4
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-03 17:25:22 +0100

    Change return view

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,6 +9,6 @@ class ArticleController extends Controller
 {
     public function show(Article $article) {
         
-        return view('admin.show', compact('article'));
+        return view('admin.article.show', compact('article'));
     }
 }
