43857f7fed455904bedc2382ee198e096b2d9196
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-04 15:10:35 +0100

    Category views

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5 pt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-white" style="background-color: #575A57;">Create Category</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('categories.store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control" name="name" autofocus>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-5">
+                                <button type="submit" class="btn btn-primary" style="background-color: #575A57;">
+                                    CREATE
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
