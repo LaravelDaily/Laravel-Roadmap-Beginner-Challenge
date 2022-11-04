3af31795328fe8fb94c20292c767fd3f3ae87d74
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-04 11:19:40 +0100

    Add index method CetegoryController

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        
+        return view('admin.category.index', compact('categories'));
     }
 
     /**
