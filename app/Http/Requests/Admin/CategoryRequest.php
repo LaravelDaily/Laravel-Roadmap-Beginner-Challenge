aa717981c6dbd3f645f537363a26882af5b93691
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-07 11:17:24 +0100

    Add unique name for creating category

--- app/Http/Requests/Admin/CategoryRequest.php
+++ app/Http/Requests/Admin/CategoryRequest.php
@@ -24,7 +24,7 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+            'name' => 'required|unique:categories,name'
         ];
     }
 }
